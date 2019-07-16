a42e4a73fdc0e628bc3f5ceeaab212566d221dfb
Javier Rogelio <javier@hhx3qf2.ord.office.telnyx.com>
2019-07-16 17:48:57 -0500

    Phoenix deployment

---
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -0,0 +1,14 @@
+defmodule GallowsWeb.HangmanView do
+  use GallowsWeb, :view
+
+  import GallowsWeb.Views.Helpers.GameStateHelper
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game) )
+  end
+
+end
--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -19,10 +19,12 @@ defmodule Hangman.Game do
 
     def make_move(game = %{ game_state: state}, _guess) when state in [:won, :lost] do
         game
+        |> return_with_tally()
     end
 
     def make_move(game, guess) do
         accept_move(game, guess, MapSet.member?(game.used, guess))
+        |> return_with_tally()
     end
 
     def tally(game) do
@@ -69,9 +71,11 @@ defmodule Hangman.Game do
     end
 
     defp reveal_letter(letter, _in_word = true), do: letter
-    defp reveal_letter(letter, _not_in_word), do: "_"
+    defp reveal_letter(_letter, _not_in_word), do: "_"
 
     defp maybe_won(true), do: :won
     defp maybe_won(_), do: :good_guess
+
+    defp return_with_tally(game), do: { game, tally(game) }
 
 end
